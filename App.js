962a6f11c67cf40c4baeef40182c96ff60f77372
fredybangs <alfredbangura77@gmail.com>
2022-07-04 10:52:18 +0000

    Paper theme provider

--- App.js
+++ App.js
@@ -9,17 +9,53 @@
 import React from 'react';
 import codePush from 'react-native-code-push';
 import RootNavigator from './src/routes/RootNavigator';
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+  DarkTheme as PaperDarkTheme,
+} from 'react-native-paper';
 
-let codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  updateDialog: {appendReleaseDescription: true},
-};
+// let codePushOptions = {
+//   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+//   installMode: codePush.InstallMode.ON_NEXT_RESUME,
+// };
 const App = () => {
+  // const CustomDefaultTheme = {
+  //   ...PaperDefaultTheme,
+  //   colors: {
+  //     ...PaperDefaultTheme.colors,
+  //     background: '#fff',
+  //     text: '#333333',
+  //     primary: '#6200EE',
+  //   },
+  // };
+
+  // const CustomDarkTheme = {
+  //   ...PaperDarkTheme,
+  //   colors: {
+  //     ...PaperDarkTheme.colors,
+  //     background: '#121212',
+  //     text: '#fff',
+  //     primary: '#6200EE',
+  //   },
+  // };
+
+  const theme = {
+    ...PaperDefaultTheme,
+    roundness: 2,
+    version: 3,
+    colors: {
+      ...PaperDefaultTheme.colors,
+      background: '#fff',
+      text: '#333333',
+      primary: '#42a5f5',
+    },
+  };
   return (
-    <>
+    <PaperProvider theme={theme}>
       <RootNavigator />
-    </>
+    </PaperProvider>
   );
 };
 
-export default codePush(codePushOptions)(App);
+export default codePush(App);
