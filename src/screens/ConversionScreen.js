42052325e224c8da969efc50ac2b985d54d5968c
fredybangs <alfredbangura77@gmail.com>
2022-07-04 10:51:48 +0000

    Prettier config

--- src/screens/ConversionScreen.js
+++ src/screens/ConversionScreen.js
@@ -79,11 +79,37 @@ const ConversionScreen = () => {
         });
       } else {
         setCent(false);
-        let new_val = val / 1000;
+        function toFixed(x) {
+          if (Math.abs(x) < 1.0) {
+            let e = parseInt(x.toString().split('e-')[1]);
+            if (e) {
+              x *= Math.pow(10, e - 1);
+              x = '0.' + new Array(e).join('0') + x.toString().substring(2);
+            }
+          } else {
+            let e = parseInt(x.toString().split('+')[1]);
+            if (e > 20) {
+              e -= 20;
+              x /= Math.pow(10, e);
+              x += new Array(e + 1).join('0');
+            }
+          }
+          return x;
+        }
+        function toFixedTrunc(x, n) {
+          x = toFixed(x);
+          const v = (typeof x === 'string' ? x : x.toString()).split('.');
+          if (n <= 0) return v[0];
+          let f = v[1] || '';
+          if (f.length > n) return `${v[0]}.${f.substr(0, n)}`;
+          while (f.length < n) f += '0';
+          return `${v[0]}.${f}`;
+        }
+        let new_val = toFixedTrunc(val / 1000, 2);
         let cent_val = Number(new_val.toString().split('.').pop());
 
         if (cent_val && new_val % 1 !== 0) {
-          let c_val = cent_val / 10;
+          let c_val = cent_val;
 
           setNewAmount({
             inWords: capitalizeFirstLetter(
